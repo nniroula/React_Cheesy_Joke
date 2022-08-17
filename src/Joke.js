5f8fedc8653b4cb45cbc7ad9e3cdf1891af41b95
nabin <nkbhaie2017@gmail.com>
2022-08-17 16:13:18 -0600

    refactor the functional joke component into class based component

--- src/Joke.js
+++ src/Joke.js
@@ -1,9 +1,18 @@
 import React from "react";
+// import React, { Component }  from "react";
 import "./Joke.css";
 
 function Joke({ vote, votes, text, id }) {
   const upVote = () => vote(id, +1);
   const downVote = () => vote(id, -1);
+
+// class Joke extends Component {
+//     constructor(props) {
+//         super(props);
+//         this.upVote = this.upVote.bind(this); // bind the object(instance) of the class
+//         this.downVote = this.downVote.bind(this);
+//         // this.toggleLock = this.toggleLock.bind(this);
+//     }
 
   return (
     <div className="Joke">
